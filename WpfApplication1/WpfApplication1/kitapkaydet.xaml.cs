3259dd3c58c1400b4d6584ce8df5da0f73d82e2e
muratttcn <muratcan-szk@hotmail.com>
2017-01-10 19:05:11 +0200

    veribaglantısıylauyelistesindegstermeyapildi
    veribaglantısıylauyelistesindegstermeyapildi

--- WpfApplication1/WpfApplication1/kitapkaydet.xaml.cs
+++ WpfApplication1/WpfApplication1/kitapkaydet.xaml.cs
@@ -11,6 +11,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+
 
 namespace WpfApplication1
 {
@@ -19,10 +23,13 @@ namespace WpfApplication1
     /// </summary>
     public partial class kitapkaydet : Window
     {
+        
+
         public kitapkaydet()
         {
             InitializeComponent();
         }
+      
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -30,5 +37,28 @@ namespace WpfApplication1
             anasayfa.Show();
             this.Hide();
         }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MySqlConnection baglati = new MySqlConnection("Server=localhost;Port=3306;Database=test;Uid=muratcn5734;");
+            try
+            {
+                baglati.Open();
+            }
+            catch (MySqlException hata)
+            {
+                MessageBox.Show(hata.Message);
+                this.Close();
+                return;
+            }
+           
+
+            MySqlCommand komut = new MySqlCommand("INSERT INTO kitapeklem (barkodnumarası,kitapadi,kitapturu,basimtarihi,yazar,basımsayisi,yayınevi,sayfasayisi,kitapucret,stok) VALUES (txt1.Text,txt2.Text,txt3.Text,txt4.Text,txt5.Text,txt6.Text,txt7.Text,txt8.Text,txt9.Text", baglati);
+            komut.ExecuteNonQuery();
+            komut.Dispose();
+
+        }
+
     }
 }
+
--- WpfApplication1/WpfApplication1/uyelistesi.xaml.cs
+++ WpfApplication1/WpfApplication1/uyelistesi.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data;
+using MySql.Data.MySqlClient;
+
 
 namespace WpfApplication1
 {
@@ -24,6 +27,8 @@ namespace WpfApplication1
             InitializeComponent();
         }
 
+       
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             anasayfa anasayfa = new anasayfa();
@@ -31,5 +36,30 @@ namespace WpfApplication1
             this.Hide();
         }
 
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MySqlConnection baglati = new MySqlConnection("Server=localhost;Port=3306;Database=test;Uid=muratcn5734;");
+            try
+            {
+                baglati.Open();
+            }
+            catch (MySqlException hata)
+            {
+                MessageBox.Show(hata.Message);
+                this.Close();
+                return;
+            }
+
+
+            MySqlCommand komut = new MySqlCommand("SELECT * FROM test", baglati);
+
+
+            MySqlDataAdapter adabtor = new MySqlDataAdapter(komut);
+            DataTable tablo = new DataTable();
+            adabtor.Fill(tablo);
+
+
+            dg1.ItemsSource = tablo.AsDataView();
+        }
     }
 }
