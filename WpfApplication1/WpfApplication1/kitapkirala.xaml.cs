f16f2fe9906bf3f73b3e0bdba3287a3c30d088da
muratttcn <muratcan-szk@hotmail.com>
2017-01-10 19:51:00 +0200

    veribaglantisiylakitapkiralamayapildi
    veribaglantisiylakitapkiralamayapildi

--- WpfApplication1/WpfApplication1/kitapkirala.xaml.cs
+++ WpfApplication1/WpfApplication1/kitapkirala.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data;
+using MySql.Data.MySqlClient;
 
 namespace WpfApplication1
 {
@@ -29,6 +31,32 @@ namespace WpfApplication1
             anasayfa anasayfa = new anasayfa();
             anasayfa.Show();
             this.Hide();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MySqlConnection baglati = new MySqlConnection("Server=localhost;Port=3306;Database=test;Uid=muratcn5734;");
+            try
+            {
+                baglati.Open();
+            }
+            catch (MySqlException hata)
+            {
+                MessageBox.Show(hata.Message);
+                this.Close();
+                return;
+            }
+
+
+            MySqlCommand komut = new MySqlCommand("SELECT * FROM kitapkirayanlar", baglati);
+
+
+            MySqlDataAdapter adabtor = new MySqlDataAdapter(komut);
+            DataTable tablo = new DataTable();
+            adabtor.Fill(tablo);
+
+
+            dg1.ItemsSource = tablo.AsDataView();
         }
     }
 }
